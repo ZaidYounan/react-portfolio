7e4256dbc0250eab65f2d7c8c9be195cf8419d4f
ZaidYounan <zaid.younan@gmail.com>
2021-08-09 00:16:10 +1000

    Adjust index text, change background color on Home/Projects/Contact, style home text

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -8,13 +8,15 @@ function Home() {
         <div className="home">
             <div className="home-text">
                 <h3>Hi, I'm Zaid.</h3>
-                <h1>React.js Developer.</h1>
-                <p>In <strong>2021</strong>, unsatisfied with my career in Pharmacy and Customer Service, I made the leap and pursued my dream of learning to code.</p>
-                <p>The pandemic was a tragedy, but it did give me the time to really knuckle down and hone my skills.</p>
-                <p>With time, my interest in coding really blossomed into a passion. I spend much of my free time tinkering, constantly learning. </p>
-                <p>Today, I have a solid base of experience with ReactJS, Ruby on Rails and related technologies.</p>
-                <p>I'm also quite interested in building API's, and I am handy with a SQL database.</p>
-                <p>Have a role that might interest me? You can  <Link to="/contact">contact me here!</Link></p>
+                <h1>Full Stack Developer.</h1>
+                <div className="inner-text">
+                    <p>In <strong>2021</strong>, unsatisfied with my career in Pharmacy and Customer Service, I made the leap and pursued my dream of learning to code.</p>
+                    <p>The COVID pandemic was a tragedy, but it gave me the time to knuckle down and hone my skills.</p>
+                    <p>With time, my interest in coding really blossomed into a true passion. I now spend much of my free time tinkering - constantly learning. </p>
+                    <p>Today, I have a solid base of experience with ReactJS, Ruby on Rails and related technologies.</p>
+                    <p>I'm also quite interested in building API's, and I am handy with a SQL database.</p>
+                    <p>Have a role that might interest me? You can  <Link to="/contact">contact me here!</Link></p>
+                </div>
             </div>
             <img src={image} className="home-image"/>
         </div>
