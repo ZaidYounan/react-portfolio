c2c4e5baa8143f5df2eb23eb998ae276c4ba0a7b
ZaidYounan <zaid.younan@gmail.com>
2021-08-08 18:11:10 +1000

    Install react-router-dom, create and style navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/nav/Navbar'
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Navbar/>
+      </Router>
     </div>
   );
 }
