23c05db80711c94ffa04b7241934bf13c5c580e2
ZaidYounan <zaid.younan@gmail.com>
2021-08-08 23:54:46 +1000

    Add contact form, install emailjs and connect it to form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,36 @@
 import React from 'react'
 import './Contact.css'
+import emailjs from 'emailjs-com';
 
 function Contact() {
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('service_k1utqk7', 'template_exq7ava', e.target, 'user_K61V87HVCXZ8d2JSf2EXk')
+        .then((result) => {
+            console.log(result.text);
+            alert("Email sent successfully!");
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset();
+        return <div>Message sent!</div>
+    }
+
     return (
         <div className="contact">
-            Contact Me 
+            <form className="contact-form" onSubmit={sendEmail}>
+                <label>Subject</label>
+                <input type="text" name="subject" />
+                <label>Name</label>
+                <input type="text" name="name" />
+                <label>Email</label>
+                <input type="email" name="email" />
+                <label>Message</label>
+                <textarea name="message" />
+                <input type="submit" value="Send" />
+            </form>
         </div>
     )
 }
