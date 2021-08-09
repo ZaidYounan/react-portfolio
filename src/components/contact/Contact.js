ee8684c9fbb624d6c634c107b6a26298a6e2bcd3
ZaidYounan <zaid.younan@gmail.com>
2021-08-09 13:10:52 +1000

    Adjust styling and responsiveness on Home, Projects and Contact page

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -22,19 +22,20 @@ function Contact() {
     return (
         <div className="wpforms-container-full contact">
 
-            <div className="contact-text">
-                <h4>Want to get in touch? You can message me directly with the below form, or email me at <a href="mailto:zaid.younan@gmail.com">zaid.younan@gmail.com</a>.</h4>
-                <h4>Alternatively you can find me on <a href="https://github.com/ZaidYounan">Github</a> and <a href="https://www.linkedin.com/in/zaid-younan/">LinkedIn</a></h4>
-            
+            <div className="contact-div">
+                <div className="contact-text">
+                    <h4>Want to get in touch? You can message me directly with the form below, or email me at <a href="mailto:zaid.younan@gmail.com">zaid.younan@gmail.com</a>.</h4>
+                    <h4>Alternatively you can find me on <a href="https://github.com/ZaidYounan">Github <i class="fab fa-github"></i></a> and <a href="https://www.linkedin.com/in/zaid-younan/">Linked<i class="fab fa-linkedin"></i></a>.</h4>
+                </div>
 
                 <form className="wpforms-form contact-form" onSubmit={sendEmail}>
-                    <label>Subject</label>
+                    <label>Subject:</label>
                     <input type="text" name="subject" />
-                    <label>Name</label>
+                    <label>Name:</label>
                     <input type="text" name="name" />
-                    <label>Email</label>
+                    <label>Email:</label>
                     <input type="email" name="email" />
-                    <label>Message</label>
+                    <label>Message:</label>
                     <textarea name="message" />
                     <input type="submit" value="Send" />
                 </form>
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -14,28 +14,37 @@ function Projects() {
                 <div className="intro-text">
                     <h1>Previous Work</h1> 
                     <p>Gaining practical experience with full-stack frameworks and technologies...</p>
+                    <p>Focusing on practicality and responsiveness...</p>
                 </div>
             </div>
             <div className="project">
-                <img src={fightclub} className="project-image"/> 
+                <a href="https://fight-club-mz.netlify.app/" className="project-link">
+                    <img src={fightclub} className="project-image"/> 
+                </a>
                 <div className="project-text">
                     <p>Full Stack App built with ReactJS and a Rails API</p>
                 </div>
             </div>
             <div className="project">
-                <img src={imentor} className="project-image"/> 
+                <a href="https://imentorapp.herokuapp.com/" className="project-link">
+                    <img src={imentor} className="project-image"/> 
+                </a>
                 <div className="project-text">
                     <p>Marketplace App built in Rails, deployed to Heroku</p>                
                 </div>
             </div>
             <div className="project">
-                <img src={weightlogger} className="project-image"/> 
+                <a href="http://weightlogger-hackathon.herokuapp.com/" className="project-link">
+                    <img src={weightlogger} className="project-image"/> 
+                </a>
                 <div className="project-text">
                     <p>24 Hour Hackathon - First time using Ruby On Rails</p>                
                 </div>
             </div>
             <div className="project">
-                <img src={swordcrafter} className="project-image"/> 
+                <a href="https://github.com/ZaidYounan/Sword-crafting-app/" className="project-link">
+                    <img src={swordcrafter} className="project-image"/> 
+                </a>
                 <div className="project-text">
                     <p>Terminal App built in Ruby - Create custom ASCII swords based on user input</p>                
                 </div>
