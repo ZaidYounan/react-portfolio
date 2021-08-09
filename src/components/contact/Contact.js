64c04a7be44ce9585d826f22f44b790fae568b81
ZaidYounan <zaid.younan@gmail.com>
2021-08-09 12:07:49 +1000

    Flesh out contact page, implement basic styling

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -10,27 +10,35 @@ function Contact() {
         emailjs.sendForm('service_k1utqk7', 'template_exq7ava', e.target, 'user_K61V87HVCXZ8d2JSf2EXk')
         .then((result) => {
             console.log(result.text);
-            alert("Email sent successfully!");
+            alert("Message sent successfully!");
         }, (error) => {
             console.log(error.text);
+            alert("Error! Message could not be sent!")
         });
         e.target.reset();
         return <div>Message sent!</div>
     }
 
     return (
-        <div className="contact">
-            <form className="contact-form" onSubmit={sendEmail}>
-                <label>Subject</label>
-                <input type="text" name="subject" />
-                <label>Name</label>
-                <input type="text" name="name" />
-                <label>Email</label>
-                <input type="email" name="email" />
-                <label>Message</label>
-                <textarea name="message" />
-                <input type="submit" value="Send" />
-            </form>
+        <div className="wpforms-container-full contact">
+
+            <div className="contact-text">
+                <h4>Want to get in touch? You can message me directly with the below form, or email me at <a href="mailto:zaid.younan@gmail.com">zaid.younan@gmail.com</a>.</h4>
+                <h4>Alternatively you can find me on <a href="https://github.com/ZaidYounan">Github</a> and <a href="https://www.linkedin.com/in/zaid-younan/">LinkedIn</a></h4>
+            
+
+                <form className="wpforms-form contact-form" onSubmit={sendEmail}>
+                    <label>Subject</label>
+                    <input type="text" name="subject" />
+                    <label>Name</label>
+                    <input type="text" name="name" />
+                    <label>Email</label>
+                    <input type="email" name="email" />
+                    <label>Message</label>
+                    <textarea name="message" />
+                    <input type="submit" value="Send" />
+                </form>
+            </div>
         </div>
     )
 }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -13,7 +13,7 @@ function Home() {
                     <p>In <strong>2021</strong>, unsatisfied with my career in Pharmacy and Customer Service, I made the leap and pursued my dream of learning to code.</p>
                     <p>The COVID pandemic was a tragedy, but it gave me the time to knuckle down and hone my skills.</p>
                     <p>With time, my interest in coding really blossomed into a true passion. I now spend much of my free time tinkering - constantly learning. </p>
-                    <p>Today, I have a solid base of experience with ReactJS, Ruby on Rails and related technologies.</p>
+                    <p>Today, I have a solid base of experience with ReactJS, Ruby on Rails and other related technologies.</p>
                     <p>I'm also quite interested in building API's, and I am handy with a SQL database.</p>
                     <p>Have a role that might interest me? You can  <Link to="/contact">contact me here!</Link></p>
                 </div>
