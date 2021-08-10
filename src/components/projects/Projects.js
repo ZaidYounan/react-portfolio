e58ce2b2ed06333850a04b1e5d9fde9d78ed36ff
ZaidYounan <zaid.younan@gmail.com>
2021-08-10 23:04:13 +1000

    Add loading hook to Projects with setTimeout 2000ms (to give images time to load), adjust responsiveness

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './Projects.css';
-import image from '../../images/linkedin-photo-render.png';
 import fightclub from '../../images/fight-club.png';
 import imentor from '../../images/imentor.png';
 import weightlogger from '../../images/weightlogger.png';
 import swordcrafter from '../../images/swordcrafter.png';
+import { useState } from 'react';
 
 
 function Projects() {
+    const [ loading, setLoading ] = useState(false);
+    
+    setTimeout(() => {
+        setLoading(true);
+      }, 2000);
+
     return (
         <div className="projects-container">
             <div className="projects-intro">
@@ -17,58 +23,61 @@ function Projects() {
                     <p>Focusing on practicality and responsiveness...</p>
                 </div>
             </div>
-            <div className="project">
-                <a href="https://fight-club-mz.netlify.app/" className="project-link">
-                    <img src={fightclub} className="project-image"/> 
-                </a>
-                <div className="project-text">
-                    <p>
-                        Fight Club: Full Stack App built with ReactJS and a Rails API. 
-                    </p>
-                    <a href="https://fight-club-mz.netlify.app/" className="project-link-text">
-                        Learn More <i class="fas fa-chevron-right"></i>
-                    </a>
-                </div>
-            </div>
-            <div className="project">
-                <a href="https://imentorapp.herokuapp.com/" className="project-link">
-                    <img src={imentor} className="project-image"/> 
-                </a>
-                <div className="project-text">
-                    <p>
-                        iMentor: Two-way Marketplace App built in Rails, deployed to Heroku.
-                    </p>          
-                    <a href="https://imentorapp.herokuapp.com/" className="project-link-text">
-                        Learn More <i class="fas fa-chevron-right"></i>
-                    </a>      
-                </div>
-            </div>
-            <div className="project">
-                <a href="http://weightlogger-hackathon.herokuapp.com/" className="project-link">
-                    <img src={weightlogger} className="project-image"/> 
-                </a>
-                <div className="project-text">
-                    <p>
-                        WeightLogger (24 Hour Hackathon) - First time using Ruby On Rails
-                    </p>
-                    <a href="http://weightlogger-hackathon.herokuapp.com/" className="project-link-text">
-                        Learn More <i class="fas fa-chevron-right"></i>
-                    </a>                
-                </div>
-            </div>
-            <div className="project">
-                <a href="https://github.com/ZaidYounan/Sword-crafting-app/" className="project-link">
-                    <img src={swordcrafter} className="project-image"/> 
-                </a>
-                <div className="project-text">
-                    <p>
-                        SwordCrafter - Terminal App built in Ruby: Create custom ASCII swords based on user input
-                    </p>          
-                    <a href="https://github.com/ZaidYounan/Sword-crafting-app/" className="project-link-text">
-                        Learn More <i class="fas fa-chevron-right"></i>
-                    </a>      
-                </div>
-            </div>
+                {!!loading ? (
+                    <div>
+                    <div className="project">
+                        <a href="https://fight-club-mz.netlify.app/" className="project-link">
+                            <img src={fightclub} className="project-image"/> 
+                        </a>
+                        <div className="project-text">
+                            <p>
+                                Fight Club: Full Stack App built with ReactJS and a Rails API. 
+                            </p>
+                            <a href="https://fight-club-mz.netlify.app/" className="project-link-text">
+                                Learn More <i class="fas fa-chevron-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="project">
+                        <a href="https://imentorapp.herokuapp.com/" className="project-link">
+                            <img src={imentor} className="project-image"/> 
+                        </a>
+                        <div className="project-text">
+                            <p>
+                                iMentor: Two-way Marketplace App built in Rails, deployed to Heroku.
+                            </p>          
+                            <a href="https://imentorapp.herokuapp.com/" className="project-link-text">
+                                Learn More <i class="fas fa-chevron-right"></i>
+                            </a>      
+                        </div>
+                    </div>
+                    <div className="project">
+                        <a href="http://weightlogger-hackathon.herokuapp.com/" className="project-link">
+                            <img src={weightlogger} className="project-image"/> 
+                        </a>
+                        <div className="project-text">
+                            <p>
+                                WeightLogger (24 Hour Hackathon) - First time using Ruby On Rails
+                            </p>
+                            <a href="http://weightlogger-hackathon.herokuapp.com/" className="project-link-text">
+                                Learn More <i class="fas fa-chevron-right"></i>
+                            </a>                
+                        </div>
+                    </div>
+                    <div className="project">
+                        <a href="https://github.com/ZaidYounan/Sword-crafting-app/" className="project-link">
+                            <img src={swordcrafter} className="project-image"/> 
+                        </a>
+                        <div className="project-text">
+                            <p>
+                                SwordCrafter - Terminal App built in Ruby: Create custom ASCII swords based on user input
+                            </p>          
+                            <a href="https://github.com/ZaidYounan/Sword-crafting-app/" className="project-link-text">
+                                Learn More <i class="fas fa-chevron-right"></i>
+                            </a>      
+                        </div>
+                    </div>
+                    </div>) : (<div className="loading-text"><h1>Loading...</h1></div>)}
             <div className="extra">&nbsp;</div>
         </div>
     )
